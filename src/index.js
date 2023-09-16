d78000b8949033e2c9b74b2014958c50a2f536a2
Nikhil Singh <ns2125560@gmail.com>
2023-09-16 11:16:34 +0530

    index files changes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { Provider as ReduxProvider } from "react-redux";
 import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <ReduxProvider store={store}>
-    <App />
-  </ReduxProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>
 );
